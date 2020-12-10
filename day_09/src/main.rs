5f0c8efd6b1887c13bff0449c7b0a097dc50e0bb
Jonathan Cammisuli <jon@cammisuli.ca>
2020-12-10 17:12:47 -0500

    day 09 part 2

--- day_09/src/main.rs
+++ day_09/src/main.rs
@@ -4,9 +4,10 @@ use std::fmt::Display;
 fn main() {
     println!("Hello, world!");
     let numbers = inputs();
-
+    let part_one = part_one(&numbers);
     //675280050
-    println!("{}", part_one(&numbers));
+    println!("{}", part_one);
+    println!("{}", part_two(part_one, &numbers));
 }
 
 fn inputs() -> Vec<usize> {
@@ -16,7 +17,7 @@ fn inputs() -> Vec<usize> {
         .collect::<Vec<_>>()
 }
 
-fn part_one(numbers: &[usize]) -> impl Display {
+fn part_one(numbers: &[usize]) -> usize {
     numbers
         // windows takes a length, while numbers is a index
         .windows(26)
@@ -27,4 +28,20 @@ fn part_one(numbers: &[usize]) -> impl Display {
         })
         .map(|nums| nums[25])
         .unwrap()
+}
+
+fn part_two(target: usize, numbers: &[usize]) -> impl Display {
+    let (mut i, mut j, mut sum) = (0, 0, 0);
+    while sum != target {
+        if sum < target {
+            sum += numbers[j];
+            j += 1;
+        } else {
+            sum -= numbers[i];
+            i += 1;
+        }
+    }
+    let min = numbers[i..j].iter().min().unwrap();
+    let max = numbers[i..j].iter().max().unwrap();
+    min + max
 }
